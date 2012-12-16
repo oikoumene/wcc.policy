9ed814f41878fb331d9db2dc8e61b9f94979f8b6
Izhar Firdaus <izhar@inigo-tech.com>
2012-12-17 03:46:20 +0800

    add dependencies

--- wcc/policy/__init__.py
+++ wcc/policy/__init__.py
@@ -1,5 +1,10 @@
 from zope.interface import implements
 from Products.CMFQuickInstallerTool.interfaces import INonInstallable
+from zope.i18nmessageid import MessageFactory
+
+# Set up the i18n message factory for our package
+MessageFactory = MessageFactory('wcc.policy')
+
 
 class HiddenProducts(object):
     """This hides the upgrade profiles from the quick installer tool."""
